9e33858e55636d5dd4808d401202ff5c69029ae2
hirensakhiya <hirenh.sakhiya@gmail.com>
2019-08-05 11:12:25 +0530

    Initial Commit

---
+++ MVVMTableviewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMTableviewDemo
+//
+//  Created by Qodors on 05/08/19.
+//  Copyright © 2019 Qodors. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
